650101bb26478a18735853431af725045751b3e1
Phi Tran <phitran@Phis-MBP.attlocal.net>
2019-02-15 11:37:16 -0600

    added play again button

--- colorgame.js
+++ colorgame.js
@@ -4,10 +4,28 @@ var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.getElementById("message");
+var h1 = document.querySelector("h1");
+
+var resetButton = document.querySelector("#reset");
+resetButton.addEventListener("click", function(){
+  
+    //generate new colors 
+    colors = generateRandomColors(6);
+
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+
+    //changing colors of our squares 
+    for (var i = 0; i < squares.length; i++){
+        squares[i].style.backgroundColor = colors[i];
+    }
+    h1.style.backgroundColor = "#232323"
+})
 
 colorDisplay.textContent = pickedColor;
 
 for(var i = 0; i < squares.length; i++){
+    //adding colors to our squares
     squares[i].style.backgroundColor = colors[i];
 
     //click listeners
@@ -17,6 +35,8 @@ for(var i = 0; i < squares.length; i++){
        if(clickedColor === pickedColor){
            messageDisplay.textContent = "Correct!";
            changeColors(clickedColor);
+           h1.style.backgroundColor = clickedColor;
+           resetButton.textContent = "Play Again?";
        }else {
            this.style.backgroundColor = "#232323";
            messageDisplay.textContent = "Try Again!";
@@ -25,11 +45,14 @@ for(var i = 0; i < squares.length; i++){
 }
 
 function changeColors(color){
+    //looping through all squares
     for (var i=0; i < squares.length; i++) {
+        //changing each color to match the given color
         squares[i].style.backgroundColor = color;
     }
 }
 
+//this function is picking a random color in our colors array
 function pickColor(){
    var random = Math.floor(Math.random() * colors.length);
    return colors[random];
@@ -55,7 +78,7 @@ function randomColor() {
     //generating a number for blue between 0 - 255
     var b = Math.floor(Math.random() * 256)
     //returning the numbers in rgb format
-    return "rgb("+ r + "," + g + "," + b + ")";
+    return "rgb("+ r + ", " + g + ", " + b + ")";
 
 }
 
